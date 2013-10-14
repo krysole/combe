4ad3bad37fae1731e2db5d02a40fe835d6e4a4d5
Lorenz Pretterhofer <krysole@alexicalmistake.com>
2013-10-14 11:41:42 +1030

    Changed indexOfPredicate to return -1 on failures similarly to indexOf

--- Runtime/stage0/Array.js
+++ Runtime/stage0/Array.js
@@ -198,7 +198,7 @@ Array.extend({
     for (var i = 0; i < this.length; i++) {
       if (predicate(this[i])) return i;
     }
-    return null;
+    return -1;
   },
   
   splitLast: function (predicateOrIndex) {
